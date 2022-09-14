0248389eaf87e3428ddc908fd73c34a5d10f7506
Silation <cho2ses@postech.ac.kr>
2022-09-14 22:21:50 +0900

    assignment

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -27,10 +27,42 @@ import scala.annotation.tailrec
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    
+    @tailrec
+    def inner_balance(chars: List[Char], left_paren_stack: Int): Boolean = {
+      if(chars.isEmpty && left_paren_stack == 0) true
+      else if(chars.isEmpty && left_paren_stack != 0) false
+      else if(left_paren_stack < 0) false
+      
+      else if(chars.head == '(')
+        inner_balance(chars.tail, left_paren_stack + 1)
+      else if(chars.head == ')')
+        inner_balance(chars.tail, left_paren_stack - 1)
+      else
+        inner_balance(chars.tail, left_paren_stack)
+      
+    }
+    
+    inner_balance(chars, 0)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def innerChange(money: Int, coins: List[Int], method: Int): Int = {
+      
+      if (money < 0) 0
+      else if (money == 0) 1
+      
+      else{
+        if (coins.isEmpty) 0
+        else
+          innerChange(money - coins.head, coins, method) + innerChange(money, coins.tail, method)
+      }
+    }
+    
+    innerChange(money, coins, 0)
+  }
 }
